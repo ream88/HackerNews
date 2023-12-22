e7406cf8102275c5dc26ef61d201f202c6d86134
Mario Uher <uher.mario@gmail.com>
2023-12-22 12:16:58 +0100

    Initialize app

---
+++ HackerNews/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HackerNews
+//
+//  Created by Mario Uher on 22.12.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HackerNews/HackerNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackerNewsApp.swift
+//  HackerNews
+//
+//  Created by Mario Uher on 22.12.23.
+//
+
+import SwiftUI
+
+@main
+struct HackerNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
